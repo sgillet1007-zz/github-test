b991a241d786c2cbe48b06cb3e278186c1e1ccb8
Sam <samuel.p.gillet@gmail.com>
2015-09-18 17:47:10 -0600

    ejs to jade

--- app.js
+++ app.js
@@ -11,58 +11,32 @@ var methodOverride = require('method-override');
 var GITHUB_CLIENT_ID = "52e94e8a791c959da470";
 var GITHUB_CLIENT_SECRET = "9f218f39a652ee06ea5ce469ba6d2e027269a073";
 
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
-//   and deserialized.
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
-
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-
-// Use the GitHubStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
-//   profile), and invoke a callback with a user object.
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:3000/auth/github/callback"
   },
+
   function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
     process.nextTick(function () {
-      
-      // To keep the example simple, the user's GitHub profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
-      // and return that user instead.
       return done(null, profile);
     });
   }
 ));
 
-
-
-
 var app = express();
-
 // configure Express
 app.use(function() {
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.logger());
-    // app.use(express.cookieParser());
-    // app.use(express.bodyParser());
-    // app.use(express.session({ secret: 'keyboard cat' }));
   app.use(cookieParser());
   app.use(bodyParser.urlencoded({extended:true}));
   app.use(bodyParser.json());
@@ -77,7 +51,6 @@ app.use(function() {
   app.use(express.static(__dirname + '/public'));
 });
 
-
 app.get('/', function(req, res){
   res.render('index', { user: req.user });
 });
@@ -90,11 +63,6 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-// GET /auth/github
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  The first step in GitHub authentication will involve redirecting
-//   the user to github.com.  After authorization, GitHubwill redirect the user
-//   back to this application at /auth/github/callback
 app.get('/auth/github',
   passport.authenticate('github'),
   function(req, res){
@@ -102,11 +70,6 @@ app.get('/auth/github',
     // function will not be called.
   });
 
-// GET /auth/github/callback
-//   Use passport.authenticate() as route middleware to authenticate the
-//   request.  If authentication fails, the user will be redirected back to the
-//   login page.  Otherwise, the primary route function function will be called,
-//   which, in this example, will redirect the user to the home page.
 app.get('/auth/github/callback', 
   passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
@@ -119,16 +82,12 @@ app.get('/logout', function(req, res){
 });
 
 // app.listen(3000);
-var port = process.env.PORT || 3000;
+var port = 3000;
 var server = app.listen(port, function(){
   console.log('Express server listening on port ' + server.address().port);
 });
 
 // Simple route middleware to ensure user is authenticated.
-//   Use this route middleware on any resource that needs to be protected.  If
-//   the request is authenticated (typically via a persistent login session),
-//   the request will proceed.  Otherwise, the user will be redirected to the
-//   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
